eea6c3231aa3a13af50c610a429d388198576c42
Lekshmi Priya B <lekshmipriya.b.2023@iimu.ac.in>
2024-05-03 00:30:39 +0530

    added try catch in backend - fixed #10

--- buji-backend/index.js
+++ buji-backend/index.js
@@ -5,8 +5,8 @@ const { VertexAI } = require('@google-cloud/vertexai');
 var fs = require('fs');
 var cors = require('cors');
 app.use(cors());
-app.use(express.json({type: "*/*", limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ type: "*/*", limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 // Initialize Vertex with your Cloud project and location
 const vertex_ai = new VertexAI({ project: 'brave-monitor-420011', location: 'us-central1' });
@@ -42,28 +42,35 @@ const generativeModel = vertex_ai.preview.getGenerativeModel({
 
 // function to generate quiz and essay based on the parameters set by the user
 async function generateContent(request, res) {
-  const streamingResp = await generativeModel.generateContentStream(request);
-  let response = "";
+  try {
+    const streamingResp = await generativeModel.generateContentStream(request);
+    let response = "";
 
-  for await (const item of streamingResp.stream) {
-    response += (item.candidates[0]?.content?.parts[0]?.text);
+    for await (const item of streamingResp.stream) {
+      response += (item.candidates[0]?.content?.parts[0]?.text);
+    }
+    response = JSON.parse(response.substr(response.indexOf("{"), response.lastIndexOf("}") - response.indexOf("{") + 1));
+    res.setHeader('Access-Control-Allow-Origin', 'https://lekib2521.github.io');
+    res.send({ response });
+
+  } catch (error) {
+    console.error(error);
+    res.setHeader('Access-Control-Allow-Origin', 'https://lekib2521.github.io');
+    res.send({ error: 1 });
   }
-  response = JSON.parse(response.substr(response.indexOf("{"), response.lastIndexOf("}") - response.indexOf("{") + 1));
-  res.setHeader('Access-Control-Allow-Origin', 'https://lekib2521.github.io');
-  res.send({ response });
 }
 
 // get function to generate quiz
 app.get('/quiz', (req, res) => {
   const request = {
     contents: [{
-        role: 'user', 
-        parts: [{
-          text: `Generate ${req.query.qno} multiple choice questions on the topic of ${req.query.text} of difficulty level ${req.query.difficulty} in the following format, I should be able to parse the questions and their options as JSON from the response.:
+      role: 'user',
+      parts: [{
+        text: `Generate ${req.query.qno} multiple choice questions on the topic of ${req.query.text} of difficulty level ${req.query.difficulty} in the following format, I should be able to parse the questions and their options as JSON from the response.:
           1:{question: Question text, a: {text: "Option 1"}, b: {text: "Option 2"}, c: {text: Option 3}, d: {text: Option 4}, correctAnswer: a},
           2:{question: Question text, a: {text: "Option 1"}, b: {text: "Option 2"}, c: {text: Option 3}, d: {text: Option 4}, correctAnswer: a}`
-        }],
       }],
+    }],
   };
   generateContent(request, res)
 });
@@ -72,27 +79,35 @@ app.get('/quiz', (req, res) => {
 app.get('/essay', (req, res) => {
   const request = {
     contents: [{
-        role: 'user', 
-        parts: [{
-          text: `Generate an essay on the topic of ${req.query.topic} as if it was written by ${req.query.author} intended to be read by ${req.query.reader}. The purpose of the essay is ${req.query.purpose}. The tone of the essay should be ${req.query.tone} and the essay should be ${req.query.wordcount} words long. Refer to ${req.query.sample} for the writing style.
+      role: 'user',
+      parts: [{
+        text: `Generate an essay on the topic of ${req.query.topic} as if it was written by ${req.query.author} intended to be read by ${req.query.reader}. The purpose of the essay is ${req.query.purpose}. The tone of the essay should be ${req.query.tone} and the essay should be ${req.query.wordcount} words long. Refer to ${req.query.sample} for the writing style.
           I should be able to parse the output as a JSON in the following format:{body: "Essay text",title: "Essay title"}`
-        }],
       }],
+    }],
   };
   generateContent(request, res)
 });
 
 // function to generate notes based on the file provided by the user
 async function generateNotes(request, res) {
-  const streamingResp = await generativeModel.generateContentStream(request);
-  let response = "";
+  try {
+    const streamingResp = await generativeModel.generateContentStream(request);
+    let response = "";
 
-  for await (const item of streamingResp.stream) {
-    response += (item.candidates[0]?.content?.parts[0]?.text);
+    for await (const item of streamingResp.stream) {
+      response += (item.candidates[0]?.content?.parts[0]?.text);
+    }
+    response = { notes: response };
+    res.setHeader('Access-Control-Allow-Origin', 'https://lekib2521.github.io');
+    res.send({ response });
+
+  } catch (error) {
+    console.error(error);
+    res.setHeader('Access-Control-Allow-Origin', 'https://lekib2521.github.io');
+    res.send({ error: 1 });
   }
-  response = {notes:response};
-  res.setHeader('Access-Control-Allow-Origin', 'https://lekib2521.github.io');
-  res.send({ response });
+
 }
 
 // get function to generate notes
@@ -108,7 +123,7 @@ app.put('/notes', (req, res) => {
     contents: [
       {
         role: 'user', parts: [image1, {
-        text: `Generate detailed study notes for the content in the given image. The output should be in plain text without bold subtitles or bullet points. Output should not contain any special characters or text formatting. 
+          text: `Generate detailed study notes for the content in the given image. The output should be in plain text without bold subtitles or bullet points. Output should not contain any special characters or text formatting. 
       The output should be a string`}]
       },
     ],
